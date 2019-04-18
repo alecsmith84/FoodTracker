71882c2d2bdf5e401e9bbc2bada9aa8d20dace36
alecsmith84 <astonmartindbrs9>
2019-04-17 20:21:05 -0500

    Initial Commit

---
+++ FoodTrackerTake2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTrackerTake2
+//
+//  Created by Alec Smith on 4/17/19.
+//  Copyright © 2019 Alec Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
